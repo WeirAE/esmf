4413cb91dc3a3cc48c94ef82bd3b66956568c417
Brian Weir <94982354+WeirAE@users.noreply.github.com>
2022-06-21 09:52:19 -0500

    Update ESMF_AltGridCreateUTest.F90

--- src/Infrastructure/Grid/tests/ESMF_AltGridCreateUTest.F90
+++ src/Infrastructure/Grid/tests/ESMF_AltGridCreateUTest.F90
@@ -115,11 +115,108 @@ program ESMF_AltGridCreateUTest
   !-----------------------------------------------------------------------------
   
   !NEX_UTest
-  write(name, *) "Testing GridCreateMosaic with different CoordTypeKind "
+  write(name, *) "Testing ATM GridCreateMosaic with different CoordTypeKind "
   write(failMsg, *) "Returns incorrect results"
 
   rc = ESMF_SUCCESS
   write(mosaicIn, *) "./data/C48_mosaic.nc"
+
+  staggerLocList(1) = ESMF_STAGGERLOC_CENTER
+  staggerLocList(2) = ESMF_STAGGERLOC_CORNER
+  threshhold = 1.0E-5
+  ! Create cubed sphere grid with coordTypeKind == ESMF_TYPEKIND_R4
+  print *, 'Reading R4 file ', mosaicIn
+  grid = ESMF_GridCreateMosaic(filename=mosaicIn, &
+                staggerLocList= staggerLocList, &
+                coordTypeKind = ESMF_TYPEKIND_R4, &
+                tileFilePath='./data/', rc=localrc)
+
+#ifndef ESMF_NETCDF
+  write(failMsg, *) "Did not return ESMF_RC_LIB_NOT_PRESENT"
+  call ESMF_Test((localrc==ESMF_RC_LIB_NOT_PRESENT), name, failMsg, result, ESMF_SRCLINE) 
+#else
+  if (localrc .ne. ESMF_SUCCESS) rc=ESMF_FAILURE
+
+  call ESMF_GridGet(grid, distgrid = distgrid, rc=localrc)
+  if (localrc .ne. ESMF_SUCCESS) rc=ESMF_FAILURE
+
+  call ESMF_DistGridGet(distgrid, delayout = delayout, rc=localrc)
+  if (localrc .ne. ESMF_SUCCESS) rc=ESMF_FAILURE
+ 
+  call ESMF_DELayoutGet(delayout, localDeCount = decount, rc=localrc)
+  if (localrc .ne. ESMF_SUCCESS) rc=ESMF_FAILURE
+
+  ! Create cubed sphere grid with coordTypeKind == ESMF_TYPEKIND_R8
+  print *, 'Reading R8 file ', mosaicIn
+  !call ESMF_MeshWrite(mosaicIn, 'ATMmosaicIn.vtk', rc=localrc)
+  grid2 = ESMF_GridCreateMosaic(filename=mosaicIn, &
+                staggerLocList= staggerLocList, &
+                coordTypeKind = ESMF_TYPEKIND_R8, &
+                tileFilePath='./data/', rc=localrc)
+  if (localrc .ne. ESMF_SUCCESS) rc=ESMF_FAILURE
+  
+  !call ESMF_MeshWrite(grid2, 'ATMmosaic2grid.vtk', rc=localrc)
+  
+  do s = 1, 2
+    do localDe = 0, decount-1  
+      call ESMF_GridGetCoord(grid2, coordDim=1, localDe=localDe, &
+         staggerloc=staggerLocList(s), farrayPtr=lonPtrR8, rc=localrc)
+      if (localrc .ne. ESMF_SUCCESS) rc=ESMF_FAILURE
+      call ESMF_GridGetCoord(grid2, coordDim=2, localDe=localDe, &
+         staggerloc=staggerLocList(s), farrayPtr=latPtrR8, rc=localrc)
+      if (localrc .ne. ESMF_SUCCESS) rc=ESMF_FAILURE
+      call ESMF_GridGetCoord(grid, coordDim=1, localDe=localDe, &
+       staggerloc=staggerLocList(s), farrayPtr=lonPtrR4, rc=localrc)
+      if (localrc .ne. ESMF_SUCCESS) rc=ESMF_FAILURE
+      call ESMF_GridGetCoord(grid, coordDim=2, localDe=localDe, &
+         staggerloc=staggerLocList(s), farrayPtr=latPtrR4, rc=localrc)
+      if (localrc .ne. ESMF_SUCCESS) rc=ESMF_FAILURE
+
+      allocate(lonDiff(size(lonPtrR8,1), size(lonPtrR8,2)))
+      allocate(latDiff(size(lonPtrR8,1), size(lonPtrR8,2)))
+      total = size(lonPtrR8,1)*size(lonPtrR8,2)
+      lonDiff = abs(lonPtrR8-lonPtrR4)
+      latDiff = abs(latPtrR8-latPtrR4)
+
+      ! Find the max/min/mean errors
+      lonmin = minval(lonDiff)
+      latmin = minval(latDiff)
+      lonmax = maxval(lonDiff)
+      latmax = maxval(latDiff)
+      allocate(mean(size(lonDiff,2)))
+      do  j=1, size(lonDiff,2)
+         mean(j) = sum(lonDiff(:,j))
+      enddo
+      lonmean = sum(mean)/total
+      do j=1, size(latDiff,2)
+         mean(j) = sum(latDiff(:,j))
+      enddo
+      latmean = sum(mean)/total
+      
+      deallocate(lonDiff, latDiff, mean)
+
+      print *, localPet, localDe, 'min/max/mean:', lonmin, latmin, lonmax, latmax, lonmean, latmean
+      if (lonmean > threshhold .or. latmean > threshhold) rc = ESMF_FAILURE
+    enddo
+  enddo
+
+  ! destroy grid
+  call ESMF_GridDestroy(grid,rc=localrc)
+  if (localrc .ne. ESMF_SUCCESS) rc=ESMF_FAILURE
+  call ESMF_GridDestroy(grid2,rc=localrc)
+  if (localrc .ne. ESMF_SUCCESS) rc=ESMF_FAILURE
+
+  call ESMF_Test((rc==ESMF_SUCCESS), name, failMsg, result, ESMF_SRCLINE)
+#endif
+
+ !-----------------------------------------------------------------------------
+  
+  !NEX_UTest
+  write(name, *) "Testing ocean GridCreateMosaic with different CoordTypeKind "
+  write(failMsg, *) "Returns incorrect results"
+
+  rc = ESMF_SUCCESS
+  write(mosaicIn, *) "./data/ocean_mosaic.nc"
 
   staggerLocList(1) = ESMF_STAGGERLOC_CENTER
   staggerLocList(2) = ESMF_STAGGERLOC_CORNER
@@ -148,14 +245,14 @@ program ESMF_AltGridCreateUTest
 
   ! Create cubed sphere grid with coordTypeKind == ESMF_TYPEKIND_R8
   print *, 'Reading file ', mosaicIn
-  !call ESMF_MeshWrite(mosaicIn, 'mosaicIn.vtk', rc=localrc)
+  !call ESMF_MeshWrite(mosaicIn, 'OCNmosaicIn.vtk', rc=localrc)
   grid2 = ESMF_GridCreateMosaic(filename=mosaicIn, &
                 staggerLocList= staggerLocList, &
                 coordTypeKind = ESMF_TYPEKIND_R8, &
                 tileFilePath='./data/', rc=localrc)
   if (localrc .ne. ESMF_SUCCESS) rc=ESMF_FAILURE
   
-  call ESMF_MeshWrite(grid2, 'mosaic2grid.vtk', rc=localrc)
+  !call ESMF_MeshWrite(grid2, 'OCNmosaic2grid.vtk', rc=localrc)
   
   do s = 1, 2
     do localDe = 0, decount-1  
@@ -211,10 +308,10 @@ program ESMF_AltGridCreateUTest
 
   !-----------------------------------------------------------------------------
   !NEX_UTest
-  write(name, *) "Test ESMF_GridCreate with different coordTypeKind with GRIDSPEC supergrid tile file"
+  write(name, *) "Test ATM ESMF_GridCreate with different coordTypeKind with GRIDSPEC supergrid tile file"
   write(failMsg, *) "Did not return ESMF_SUCCESS"
 
-  grid=ESMF_GridCreate('data/horizontal_grid.tile6.nc', &
+  grid=ESMF_GridCreate('data/C48_grid.tile6.nc', &
     ESMF_FILEFORMAT_GRIDSPEC, coordTypeKind=ESMF_TYPEKIND_R4, rc=rc)
 
 #ifdef ESMF_NETCDF
@@ -228,7 +325,80 @@ program ESMF_AltGridCreateUTest
   call ESMF_DELayoutGet(delayout, localDeCount = decount, rc=localrc)
   if (localrc .ne. ESMF_SUCCESS) rc=ESMF_FAILURE
 
-  grid2=ESMF_GridCreate('data/horizontal_grid.tile6.nc', &
+  grid2=ESMF_GridCreate('data/C48_grid.tile6.nc', &
+    ESMF_FILEFORMAT_GRIDSPEC, coordTypeKind=ESMF_TYPEKIND_R8, rc=localrc)
+  if (localrc .ne. ESMF_SUCCESS) rc=ESMF_FAILURE
+  
+  threshhold = 1.0E-5
+
+  do localDe = 0, decount-1  
+      call ESMF_GridGetCoord(grid2, coordDim=1, localDe=localDe, &
+         staggerloc=ESMF_STAGGERLOC_CENTER, farrayPtr=lonPtrR8, rc=localrc)
+      if (localrc .ne. ESMF_SUCCESS) rc=ESMF_FAILURE
+      call ESMF_GridGetCoord(grid2, coordDim=2, localDe=localDe, &
+         staggerloc=ESMF_STAGGERLOC_CENTER, farrayPtr=latPtrR8, rc=localrc)
+      if (localrc .ne. ESMF_SUCCESS) rc=ESMF_FAILURE
+      call ESMF_GridGetCoord(grid, coordDim=1, localDe=localDe, &
+       staggerloc=ESMF_STAGGERLOC_CENTER, farrayPtr=lonPtrR4, rc=localrc)
+      if (localrc .ne. ESMF_SUCCESS) rc=ESMF_FAILURE
+      call ESMF_GridGetCoord(grid, coordDim=2, localDe=localDe, &
+         staggerloc=ESMF_STAGGERLOC_CENTER, farrayPtr=latPtrR4, rc=localrc)
+      if (localrc .ne. ESMF_SUCCESS) rc=ESMF_FAILURE
+
+      allocate(lonDiff(size(lonPtrR8,1), size(lonPtrR8,2)))
+      allocate(latDiff(size(lonPtrR8,1), size(lonPtrR8,2)))
+      total = size(lonPtrR8,1)*size(lonPtrR8,2)
+      lonDiff = abs(lonPtrR8-lonPtrR4)
+      latDiff = abs(latPtrR8-latPtrR4)
+
+      ! Find the mean errors
+      allocate(mean(size(lonDiff,2)))
+      do  j=1, size(lonDiff,2)
+         mean(j) = sum(lonDiff(:,j))
+      enddo
+      lonmean = sum(mean)/total
+      do j=1, size(latDiff,2)
+         mean(j) = sum(latDiff(:,j))
+      enddo
+      latmean = sum(mean)/total
+      
+      deallocate(lonDiff, latDiff, mean)
+
+      if (lonmean > threshhold .or. latmean > threshhold) rc = ESMF_FAILURE
+  enddo
+
+  call ESMF_GridDestroy(grid,rc=localrc)
+  if (localrc .ne. ESMF_SUCCESS) rc=ESMF_FAILURE
+  call ESMF_GridDestroy(grid2,rc=localrc)
+  if (localrc .ne. ESMF_SUCCESS) rc=ESMF_FAILURE
+
+  call ESMF_Test((rc==ESMF_SUCCESS), name, failMsg, result, ESMF_SRCLINE)
+#else
+  write(failMsg, *) "Did not return ESMF_RC_LIB_NOT_PRESENT"
+  call ESMF_Test((rc==ESMF_RC_LIB_NOT_PRESENT), name, failMsg, result, ESMF_SRCLINE) 
+#endif
+
+  call ESMF_TestEnd(ESMF_SRCLINE)
+  !-----------------------------------------------------------------------------
+    !NEX_UTest
+  write(name, *) "Test OCN ESMF_GridCreate with different coordTypeKind with GRIDSPEC supergrid tile file"
+  write(failMsg, *) "Did not return ESMF_SUCCESS"
+
+  grid=ESMF_GridCreate('data/ocean_hgrid.nc', &
+    ESMF_FILEFORMAT_GRIDSPEC, coordTypeKind=ESMF_TYPEKIND_R4, rc=rc)
+
+#ifdef ESMF_NETCDF
+
+  call ESMF_GridGet(grid, distgrid = distgrid, rc=localrc)
+  if (localrc .ne. ESMF_SUCCESS) rc=ESMF_FAILURE
+
+  call ESMF_DistGridGet(distgrid, delayout = delayout, rc=localrc)
+  if (localrc .ne. ESMF_SUCCESS) rc=ESMF_FAILURE
+ 
+  call ESMF_DELayoutGet(delayout, localDeCount = decount, rc=localrc)
+  if (localrc .ne. ESMF_SUCCESS) rc=ESMF_FAILURE
+
+  grid2=ESMF_GridCreate('data/ocean_hgrid.nc', &
     ESMF_FILEFORMAT_GRIDSPEC, coordTypeKind=ESMF_TYPEKIND_R8, rc=localrc)
   if (localrc .ne. ESMF_SUCCESS) rc=ESMF_FAILURE
   
